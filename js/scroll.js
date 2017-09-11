439c2061fb2c7aac3e01529d96680cd318b50e59
Nick Chaloult <31291920+nchaloult@users.noreply.github.com>
2017-09-11 18:48:45 -0400

    Add files via upload
    Initial commit.

---
+++ js/scroll.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    
+    var offset = 500;
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, 1250);
+        return false;
+    })
+    
+    $('a[href*=#]:not([href=#])').click(function(event) {
+        event.preventDefault();
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1250);
+            return false;
+          }
+        }
+      });
+    
+});
